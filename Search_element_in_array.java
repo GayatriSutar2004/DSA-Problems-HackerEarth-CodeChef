5a3dbaec1e47116e865094585705a9625fe9b0fe
GayatriSutar2004 <gayatrisutar10170@gmail.com>
2025-08-03 01:17:44 +0530

    Create Search_element_in_array.java

---
+++ Search_element_in_array.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		int x=sc.nextInt();//size , element
+		int[] a=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+        		
+        		int f=0;
+        for(int i=0;i<n;i++){
+		   if(a[i]==x)
+		    {
+		        System.out.println("YES");
+		        f=1;
+		        break;
+            }
+            
+        }
+        
+        if(f==0)
+	        System.out.println("NO");
+
+        
+	}
+}
