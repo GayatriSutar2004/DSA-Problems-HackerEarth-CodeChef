941bf3eef1c36c2989036d6d7f774a2c16f79ebb
Gayatri Sutar <gayatrisutar10170@gmail.com>
2025-08-27 22:55:00 +0530

    Create subarraySumCount.java
    subarray sum count DSA

---
+++ subarraySumCount.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+  int c=0;
+int n=nums.lenght;
+for(int i=0;i<n;i++){
+         int s=0;
+          for(int j=i;i<n;j++){
+            s=s+nums[j];
+            if(s==k){
+              c++;
+            }
+          }
+        }
+return c;    
+}
+}
+
+
+
+
+
+----------------------------------------------------------------------
+560. Subarray Sum Equals K
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
