28983ba1a74c109ea4778b449a8a764ea6000c15
Gayatri Sutar <gayatrisutar10170@gmail.com>
2026-02-12 17:50:24 +0530

    Update maxSubArray2.java
    O(n)

--- maxSubArray2.java
+++ maxSubArray2.java
@@ -2,8 +2,8 @@ public int maxsubarray(int []nums){
   int curr=nums[0];
   int maxx=nums[0];
 
-  for(int i=0;i<nums.length;i++){
-    curr=Math.max(curr,curr+nums[i]);
+  for(int i=1;i<nums.length;i++){
+    curr=Math.max(nums[i],curr+nums[i]);
     maxx=Math.max(curr,maxx);
   }
 return maxx;
